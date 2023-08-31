cca9155b22cb24df50c9bee17697c9ebafdaed9d
Noel Bryant <noelsobryant@gmail.com>
2023-09-01 02:41:17 +0300

    set up github pages configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack configuration',
-      template: './index.html',
+      template: './src/index.html',
     }),
   ],
   output: {
